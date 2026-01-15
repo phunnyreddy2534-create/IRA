7f6912defe2c0df7325b4a846034a09f3b133471
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-15 23:57:14 +0530

    Update page.tsx

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,18 +1,25 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { supabase } from "../../../lib/supabaseClient";
 
 export default function SignupPage() {
+  const handleSignup = async (e: any) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    await supabase.auth.signUp({ email, password });
+  };
+
   return (
     <main className="container" style={{ maxWidth: "420px" }}>
-      <motion.h1
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-      >
+      <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }}>
         Create Account
       </motion.h1>
 
       <form
+        onSubmit={handleSignup}
         style={{
           marginTop: "20px",
           display: "flex",
@@ -20,9 +27,14 @@ export default function SignupPage() {
           gap: "14px",
         }}
       >
-        <input type="text" placeholder="Name" required />
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
+        <input type="text" placeholder="Name" />
+        <input name="email" type="email" placeholder="Email" required />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
         <button className="btn" type="submit">
           Sign Up
         </button>
