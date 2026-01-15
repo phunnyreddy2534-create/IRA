13adb64047c87c98cb080a6540738a4740b953f0
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-15 23:41:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import ChatWidget from "../components/ChatWidget";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <ChatWidget />
     </main>
   );
 }
