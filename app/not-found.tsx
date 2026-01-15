9756aeed32b15d4338560c3d8f88f74f16a3277f
phunnyreddy2534-create <phunnyreddy2534@gmail.com>
2026-01-15 20:28:24 +0530

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,12 @@
+// app/not-found.tsx
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div style={{ padding: "3rem", textAlign: "center" }}>
+    <div style={{ padding: "4rem", textAlign: "center" }}>
       <h1>404</h1>
       <p>Page not found</p>
+      <Link href="/">Go back home</Link>
     </div>
   );
 }
